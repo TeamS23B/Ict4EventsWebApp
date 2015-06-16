b6c8677dbcbe6bdde379c09d3a6c471b93ab6948
Jim Sanders <j.sanders@student.fontys.nl>
2015-06-16 15:15:50 +0200

    sdfghj

--- Ict4EventsWebApp/Ict4EventsWebApp/ReservationMaterial.aspx.designer.cs
+++ Ict4EventsWebApp/Ict4EventsWebApp/ReservationMaterial.aspx.designer.cs
@@ -103,6 +103,15 @@ namespace Ict4EventsWebApp {
         protected global::System.Web.UI.WebControls.Label lblPhone;
         
         /// <summary>
+        /// rfvFirstName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvFirstName;
+        
+        /// <summary>
         /// tbFirstName control.
         /// </summary>
         /// <remarks>
